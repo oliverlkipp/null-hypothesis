4bd58975c922b415febb48ac4874667295823df0
grubbbz <morignota@gmail.com>
2026-04-13 22:07:06 -0600

    Updated R script to include county label removal; Added code for Pearson's correlational analysis and multiple linear regression modelling; Added figure of model analysis.

--- data_husbandry.R
+++ data_husbandry.R
@@ -18,6 +18,11 @@ householdinc_data_clean <- householdinc_data_clean[,-2]
 teenhpv_vac_data_clean <- teenhpv_vac_data_clean[,-1]
 teenhpv_vac_data_clean <- teenhpv_vac_data_clean[,-2]
 
+#Removing "County" from county labels so that labels match
+noins_child_data_clean$County <- str_remove_all(noins_child_data_clean$County, " County")
+householdinc_data_clean$County <- str_remove_all(householdinc_data_clean$County, " County")
+teenhpv_vac_data_clean$County <- str_remove_all(teenhpv_vac_data_clean$County, " County")
+
 #Only keeping rows with measurement of child insurance rates + pivoting data
 noins_child_data_clean <- noins_child_data_clean %>% 
   filter(measure=="Children without Health Insurance")
@@ -41,3 +46,30 @@ noins_child_longer <- pivot_longer(noins_child_data_clean, -County, names_to=".v
 
 #Combine longer data frames into one
 alldata_comb <- rbind(noins_child_longer, houseinc_longer, teenhpv_longer)
+
+#Convert full data frame into a wider format for linear regression analysis
+alldata_wider <- pivot_wider(alldata_comb, names_from = measure, values_from = value)
+
+#General logical regression model generation for evaluation of the factors with most impact
+set.seed(6440)
+rnum <- runif(nrow(alldata_wider))
+traindata <- subset(alldata_wider, rnum < 0.8)
+testdata <- subset(alldata_wider, rnum >= 0.8)
+tmultimodel <- lm(formula=`Up-To-Date Percent` ~ `Household Income ($)` + `Children without Health Insurance`, data=traindata)
+tmm_predicted <- predict.lm(tmultimodel, newdata = testdata)
+tmm_error <- tmm_predicted - testdata$`Up-To-Date Percent`
+modelcor <- cor(tmm_predicted, testdata$`Up-To-Date Percent`, method = "pearson")
+varcor <- cor(traindata[, c("Household Income ($)", "Children without Health Insurance", "Up-To-Date Percent")], method = "pearson")
+mean(tmm_error)
+summary(tmultimodel)
+plot(tmultimodel)
+modelcor
+varcor
+
+#Plotting the model
+testdata$predicted <- tmm_predicted
+ggplot(testdata, aes(x = `Up-To-Date Percent`, y = predicted)) +
+  labs(x="Actual HPV Immunization Rate", y= "Predicted HPV Immunization Rate") +
+  theme_light() +
+  geom_point() +                                        # raw data points
+  geom_abline(slope = 1, intercept = 0, color = "red")
